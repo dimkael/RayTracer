0488542f50595da6b160ba141ef3c794930cca5d
dimkael <53169743+dimkael@users.noreply.github.com>
2020-11-15 19:41:45 +0300

    implemented hittable list in draw func

--- RayTracing/draw.h
+++ RayTracing/draw.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <memory>
 #include "vec3.h"
-#include "color.h"
 #include "ray.h"
+#include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "general_math.h"
 
 double viewport_width, viewport_height;
 double focal_length = 1.0;
@@ -36,15 +40,14 @@ double hit_sphere(const Point3& center, double radius, const Ray& ray) {
 	}
 }
 
-Color ray_color(const Ray& ray) {
-	double t = hit_sphere(Point3(0.0, 0.0, -1.0), 0.5, ray);
-	if (t > 0.0) {
-		Vec3 N = unit_vector(ray.at(t) - Vec3(0.0, 0.0, -1.0));
-		return 0.5 * Color(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
+Color ray_color(const Ray& ray, const Hittable& world) {
+	HitRecord hit_rec;
+	if (world.hit(ray, 0.0, inf, hit_rec)) {
+		return 0.5 * (hit_rec.normal + Color(1.0, 1.0, 1.0));
 	}
 
 	Vec3 unit_direction = unit_vector(ray.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
+	double t = 0.5 * (unit_direction.y() + 1.0);
 
 	return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
@@ -53,13 +56,17 @@ Color ray_color(const Ray& ray) {
 void draw(HDC& hdc, int image_width, int image_height) {
 	perspective(image_width, image_height);
 
+	HittableList world;
+	world.add(std::make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5));
+	world.add(std::make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100));
+
 	for (int j = image_height - 1; j >= 0; j--) {
 		for (int i = 0; i < image_width; i++) {
 			double u = double(i) / (image_width - 1.0);
 			double v = double(j) / (image_height - 1.0);
 
 			Ray ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			Color pixel_color = ray_color(ray);
+			Color pixel_color = ray_color(ray, world);
 
 			SetPixel(hdc, i, image_height - j, RGB(255.999 * pixel_color.x(), 255.999 * pixel_color.y(), 255.999 * pixel_color.z()));
 		}
